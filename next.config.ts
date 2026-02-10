813e59a2153b6510bab68e0481f672a5929a8b50
Jeffrey Plewak <carcodez1@gmail.com>
2026-02-10 01:50:10 -0500

    chore: redirect www to apex

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.jeffreyplewak.com" }],
+        destination: "https://jeffreyplewak.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
